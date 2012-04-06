fedde56a5fab6479a734e54580971dbf6eee6396
ChrisA23 <ChrisA23@gmail.com>
2012-04-06 17:22:36 -0500

    Moved around, added parallel test, added bmp for first test.

---
+++ QuickLZ.Tests/QuickLzTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace QuickLZ.Tests
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QuickLzTests
+    {
+        private Task _factory;
+
+        [Test]
+        public void Test1()
+        {
+            var qlz = new QuickLZ();
+
+            //create a stream and pass it through compress and decompress
+            //assert it is unchanged//
+            byte[] original = File.ReadAllBytes("./Flower.bmp");
+            byte[] compressedBytes; // = new byte[sizeCompressed];
+            byte[] result;
+            using (var compressed = new MemoryStream())
+            {
+                using (var lz = new QuickLZCompressionStream(compressed))
+                {
+                    lz.Write(original, 0, original.Length);
+                }
+                compressedBytes = compressed.ToArray();
+            }
+
+            result = new byte[original.Length];
+            using (var compressed = new MemoryStream(compressedBytes))
+            {
+                using (var lzd = new QuickLZDecompressionStream(compressed))
+                {
+                    lzd.Read(result, 0, result.Length);
+                }
+            }
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                Assert.AreEqual(original[i], result[i]);
+            }
+        }
+
+
+        [Test]
+        public void ParallelTest1()
+        {
+            var qlz = new QuickLZ();
+
+            //create a stream and pass it through compress and decompress
+            //assert it is unchanged//
+            byte[] original = File.ReadAllBytes("./Flower.bmp");
+            byte[] compressedBytes; // = new byte[sizeCompressed];
+
+            using (var compressed = new MemoryStream())
+            {
+                using (var lz = new QuickLZCompressionStream(compressed))
+                {
+                    lz.Write(original, 0, original.Length);
+                }
+                compressedBytes = compressed.ToArray();
+            }
+            var factory = new TaskFactory(TaskCreationOptions.LongRunning,TaskContinuationOptions.LongRunning);
+
+            var tasks = new List<Task>();
+            for (int j = 0; j < 4; j++)
+            {
+                var task = factory.StartNew(() =>
+                {
+                    byte[] result = new byte[original.Length];
+                    for (int k = 0; k < 100; k++)
+                    {
+                        using (var compressed = new MemoryStream(compressedBytes))
+                        using (var lzd = new QuickLZDecompressionStream(compressed))
+                        {
+                            lzd.Read(result, 0, result.Length);
+                        }
+                    }
+                });
+                tasks.Add(task);
+            }
+            Task.WaitAll(tasks.ToArray());
+
+
+        }
+    }
+}
---
+++ QuickLZ/ResourceExtractor.cs
@@ -0,0 +1,20 @@
+﻿namespace QuickLZ
+{
+    using System.IO;
+    using global::QuickLZ.Properties;
+
+    public static class ResourceExtractor
+    {
+        public static void ExtractResourceToFile(bool is64BitProcess, string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                using (var fs = new FileStream(filename, FileMode.Create))
+                {
+                    byte[] b = is64BitProcess ? Resources.quicklz150_64_1_1000000 : Resources.quicklz150_32_1_1000000;
+                    fs.Write(b, 0, b.Length);
+                }
+            }
+        }
+    }
+}
